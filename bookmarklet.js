2af7155909a9f72c46fbd661df6ed95b1e0183e3
Firas <firas@clinkle.com>
2012-01-24 22:52:29 -0800

    bookmarklet

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:var titleElements = document.getElementsByClassName("title");for(var i = 0; i < titleElements.length; i+=2){titleElements[i].firstChild.setAttribute("_target", "blank");}void(0);
